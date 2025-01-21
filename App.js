0e579ebe3ef1d333ead0f935164d4e1f8374a3ad
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-01-21 17:09:01 +0200

    counter added

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+const [text, setText] = useState("");
+const [number, setNumber] = useState(6);
+const [counter, setCounter] = useState(1);
+
+const handlePress = () => {
+  setCounter(counter+1)
+  if (text > number) {
+    Alert.alert("your number is higer") 
+  }
+  if (text < number){
+    Alert.alert("your numbeer is lower") 
+  }
+  if (text == number) {
+    Alert.alert("correct! it took you " + counter + " tries") ;
+  }
+}
+const changeNumber = () =>{
+  const rndInt = Math.floor(Math.random() * 100) + 1
+  setNumber(rndInt);
+  setCounter(0);
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Number between 1-100</Text>
+      <TextInput
+  placeholder='Place your number'
+  onChangeText={text => setText(text)} 
+  value={text} 
+      keyboardType="numeric"
+      />
+      <View>
+      <Button onPress={handlePress} title="Make guess" />
+      <Button onPress={changeNumber} title="Change Number" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
